6f3ec144c6c7f64fed0525724b1a0aeaecd3653a
dreamflowai <danielefilippetti06@gmail.com>
2025-11-20 14:36:42 +0100

    Update vite.config.js

--- dreamflow-ai/vite.config.js
+++ dreamflow-ai/vite.config.js
@@ -1,1 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  base: './', // importantissimo per far funzionare i file su Netlify
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@/': '/src/'
+    }
+  },
+  build: {
+    sourcemap: false
+  }
+})
 
